167830a1526e5a5ba3db931d28930c64a202835d
Katka Marcincakova <katulik137@gmail.com>
2023-02-26 16:20:34 +0100

    decor

--- src/decor.js
+++ src/decor.js
@@ -15,3 +15,10 @@ const el3 = document.getElementById('serviceCard');
 
 decor3.style.top = ((el3.getBoundingClientRect().top + document.documentElement.scrollTop - 30)) + 'px';
 decor3.style.left = ((el3.getBoundingClientRect().right - (decor3.clientWidth * 0.85))) + 'px';
+
+
+const decor4 = document.getElementById('decorHeroServices2');
+const el4 = document.getElementById('services');
+
+decor4.style.top = ((el4.getBoundingClientRect().top + document.documentElement.scrollTop - 50)) + 'px';
+decor4.style.right = (-50) + 'px';
