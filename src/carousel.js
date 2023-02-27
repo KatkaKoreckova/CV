4dfede38200844265090a588781a6970b0512f7b
Katka Marcincakova <katulik137@gmail.com>
2023-02-27 12:00:15 +0100

    projects carousel

---
+++ src/carousel.js
@@ -0,0 +1,20 @@
+let indexes = [0];
+function carouselNext(classN, index) {
+    var n = document.getElementsByClassName(classN);
+    n[indexes[index]].classList.add('hidden');
+    indexes[index] += 1;
+    
+    if (indexes[index] > n.length - 1)
+        indexes[index] = 0;
+    n[indexes[index]].classList.remove('hidden');
+}
+
+function carouselPrev(classN, index) {
+    var n = document.getElementsByClassName(classN);
+    n[indexes[index]].classList.add('hidden');
+    indexes[index] -= 1;
+    
+    if (indexes[index] < 0)
+        indexes[index] = n.length - 1;
+    n[indexes[index]].classList.remove('hidden');
+}
