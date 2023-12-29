7b5f151f7e399f10d3364376ee831abe1839d971
Katka Marcincakova <katulik137@gmail.com>
2023-12-29 12:05:44 +0100

    ojh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
+  integrations: [
+    tailwind({
       configFile: './tailwind.config.mjs',
-+      applyBaseStyles: true,
-  })],
+      applyBaseStyles: true,
+    }),
+  ],
   site: 'https://katkamarcincakova.github.io',
   base: '/',
 });
