c406dda0ea2c9577cd14b0fd5477e96314bb6a46
Katka Marcincakova <katulik137@gmail.com>
2023-12-29 23:12:57 +0100

    reinstall bez astra

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-      configFile: './tailwind.config.mjs',
-      applyBaseStyles: true,
-  })],
-  site: 'https://katkamarcincakova.github.io',
-  base: '/',
+    site: 'https://katkamarcincakova.github.io',
+    base: '/CV',
 });
