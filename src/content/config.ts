70c785d5adae910c33b3dfe29f2ba72b1b2c4c1a
Katka Marcincakova <katulik137@gmail.com>
2023-12-30 00:04:41 +0100

    decor updated path

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,11 @@ const projectsCollection = defineCollection({
     description: z.string(),
     techStack: z.array(z.string()),
     link: z.string().optional(),
+    img1: z.string(),
+    img2: z.string(),
+    img3: z.string(),
+    img4: z.string(),
+    img5: z.string(),
   }),
 });
 
