d819fd7b3be0f5c7d4a203b9421012c4c70d2dd5
Katka Marcincakova <katulik137@gmail.com>
2023-12-30 00:36:22 +0100

    new image import form collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,26 @@
 
 const projectsCollection = defineCollection({
   type: 'data',
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
     techStack: z.array(z.string()),
     link: z.string().optional(),
-    img1: z.string(),
-    img2: z.string(),
-    img3: z.string(),
-    img4: z.string(),
-    img5: z.string(),
+    img1: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    img2: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    img3: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    img4: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    img5: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
   }),
 });
 
