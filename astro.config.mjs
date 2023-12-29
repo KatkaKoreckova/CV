2e5435b7226bae2c80d9f3ce608375b673b93122
Katka Marcincakova <katulik137@gmail.com>
2023-12-29 12:28:01 +0100

    vbnl

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-      configFile: './tailwind.config.mjs',
+      configFile: 'tailwind.config.mjs',
       applyBaseStyles: true,
   })],
   site: 'https://katkamarcincakova.github.io',
