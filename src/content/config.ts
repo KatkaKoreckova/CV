35180d3ab8abebc0a2f3e1ac70557e1901aed1b5
Katka Marcincakova <katulik137@gmail.com>
2023-12-29 13:12:52 +0100

    vgiuhiojpi0-987

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 ﻿import { z, defineCollection } from 'astro:content';
 
 const projectsCollection = defineCollection({
-  type: 'content',
+  type: 'data',
   schema: z.object({
     title: z.string(),
     description: z.string(),
