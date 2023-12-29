45b4304b877fdddaf152cf378e10d710b4560580
Katka Marcincakova <katulik137@gmail.com>
2023-12-29 11:59:02 +0100

    grbfadrwyjna

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+      configFile: './tailwind.config.mjs',
++      applyBaseStyles: true,
+  })],
   site: 'https://katkamarcincakova.github.io',
   base: '/',
 });
