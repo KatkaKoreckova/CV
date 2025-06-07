f8c2037cf0df14f752bcea699a9bb25024721a64
KatkaKoreckova <katulik137@gmail.com>
2025-06-08 00:18:36 +0200

    new navbar and title

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://katkamarcincakova.github.io',
-  base: '/CV',
-  integrations: [tailwind()]
-});
+export default defineConfig({});
--- src/scripts/script.js
+++
@@ -1,7 +0,0 @@
-﻿const div = document.getElementById('mobileHero');
-const navh = document.getElementById('navbar');
-
-div.style.paddingTop = ((navh.clientHeight + 30) + 'px');
-
-//console.log(navh.clientHeight);
-//console.log(div.style.paddingTop);
